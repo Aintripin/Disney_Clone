1257038aca1ce46103ea59f5d667f716ccc16cba
Aintripin <yelisey_toyota@mail.ru>
2022-03-24 17:58:30 +0300

    done with the header DOM (kind of)

--- disney_clone/src/components/Header.js
+++ disney_clone/src/components/Header.js
@@ -8,9 +8,31 @@ function Header() {
             <NavMenu>
                 <a>
                     <img src = "./images/home-icon.svg" alt = "img"/> 
-                    <span>Home</span>
+                    <span>HOME</span>
                 </a>
+                <a>
+                    <img src = "./images/search-icon.svg" alt = "img"/> 
+                    <span>SEARCH</span>
+                </a>
+                <a>
+                    <img src = "./images/watchlist-icon.svg" alt = "img"/> 
+                    <span>WATCHLIST</span>
+                </a>
+                <a>
+                    <img src = "./images/original-icon.svg" alt = "img"/> 
+                    <span>ORIGINAL</span>
+                </a>
+                <a>
+                    <img src = "./images/movie-icon.svg" alt = "img"/> 
+                    <span>MOVIES</span>
+                </a>
+                <a>
+                    <img src = "./images/series-icon.svg" alt = "img"/> 
+                    <span>SERIES</span>
+                </a>
+
             </NavMenu>
+            <UserImg src = "../images/Aintripin_logo.png"/>
         </Nav>
     )
 }
@@ -33,14 +55,31 @@ const Logo = styled.img`
 
 const NavMenu = styled.div`
     display: flex;
+    flex: 1;                        // expands as much as possible; stretches horizontally
+    margin-left: 25px;              // adding some spacing between icons
+    align-items: center;
 
     a {
         display: flex;
         align-items: center;
         padding: 0 12px;            // 0 top & o bottom; 12 px letf & right
+        cursor: pointer;
     }
 
     img { 
         height: 20px;
     }
+
+    span {
+        font-size: 13px;
+        letter-spacing: 1.42px;
+
+    }
+`
+
+const UserImg = styled.img`
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    cursor: pointer;
 `
