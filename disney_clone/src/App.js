fb28a25e284023fd913ff311ceca80fb52fd0ddd
Aintripin <yelisey_toyota@mail.ru>
2022-04-01 01:18:38 +0300

    fixed react-router-dom bug (Switches related)

--- disney_clone/src/App.js
+++ disney_clone/src/App.js
@@ -4,11 +4,28 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import Home from "./components/Home";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Detail from "./components/Detail";
 
 function App() {
   return (
     <div className="App">
+    <Router>
       <Header />
+      <Switch>
+        <Route path = "./detail">
+          <Detail />
+        </Route>
+        <Route path = "./">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
       <Home />
     </div>
   );
