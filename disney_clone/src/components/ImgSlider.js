783d8aa77306d4fb07e6ebd0a185d03a9feefda7
Aintripin <yelisey_toyota@mail.ru>
2022-03-31 18:24:15 +0300

    a lot is going on

--- disney_clone/src/components/ImgSlider.js
+++ disney_clone/src/components/ImgSlider.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import styled from "styled-components"
+import Slider from "react-slick";
+// import Carousel from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 function ImgSlider() {
     return (
-        <div>
+        // <Carousel>
             ImgSlider
-        </div>
+        // </Carousel>
     )
 }
 
