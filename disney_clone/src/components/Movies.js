b7e820ba66e0d9a8653141c973d3940fcdf5da29
Aintripin <yelisey_toyota@mail.ru>
2022-03-31 21:30:43 +0300

    Movies: initial setup

---
+++ disney_clone/src/components/Movies.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Movies() {
+    return (
+        <Container>
+            Movies
+        </Container>
+    )
+}
+
+export default Movies
+
+
+const Container = styled.div`
+
+`
