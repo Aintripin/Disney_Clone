0e53bfcf5d5df78d859d511648632f585611a13e
Aintripin <yelisey_toyota@mail.ru>
2022-03-31 19:09:05 +0300

    fixed some bugs

--- disney_clone/src/components/ImgSlider.js
+++ disney_clone/src/components/ImgSlider.js
@@ -1,17 +1,38 @@
 import React from 'react'
 import styled from "styled-components"
-import Slider from "react-slick";
 // import Carousel from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 
 function ImgSlider() {
+    let settings = {            // slider settings
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true
+    }
     return (
-        // <Carousel>
-            ImgSlider
-        // </Carousel>
+        <Carousel {...settings}>
+            <Wrap>
+                <img src = "../../public/images/slider-badging.jpg" alt = "slider" />
+            </Wrap>
+            <Wrap>
+                <img src = "../../public/images/slider-badging.jpg" alt = "slider" />
+            </Wrap>
+        </Carousel>
     )
 }
 
 export default ImgSlider
+
+const Carousel = styled(Slider)`
+
+`
+
+const Wrap = styled.div`
+
+`
